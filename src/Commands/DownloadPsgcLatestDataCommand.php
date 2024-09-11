3fd99318d55584825015a0ec5c7b993116deeb09
Jeric June Logan <loganjeric25@gmail.com>
2024-09-11 23:39:45 +0800

    update spin

--- src/Commands/DownloadPsgcLatestDataCommand.php
+++ src/Commands/DownloadPsgcLatestDataCommand.php
@@ -5,7 +5,10 @@ namespace Jericdei\PsgcDatabase\Commands;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Storage;
+use Laravel\Prompts\Spinner;
 use Symfony\Component\DomCrawler\Crawler;
+
+use function Laravel\Prompts\spin;
 
 class DownloadPsgcLatestDataCommand extends Command
 {
@@ -25,18 +28,15 @@ class DownloadPsgcLatestDataCommand extends Command
 
         $link = $crawler->filterXPath('//a[text()="Publication"]')->first();
 
-        $spinner = $this->spinner();
-        $spinner->setMessage('Downloading latest PSGC file...');
-        $spinner->start();
+        $excel = spin(
+            message: 'Downloading latest PSGC file...',
+            callback: function () use ($link) {
+                return Http::connectTimeout(999999)
+                    ->get($link->attr('href'))
+                    ->body();
+            }
+        );
 
-        $excel = Http::connectTimeout(999999)
-            ->withOptions([
-                'progress' => fn () => $spinner->advance(),
-            ])
-            ->get($link->attr('href'))
-            ->body();
-
-        $spinner->finish();
         $this->newLine();
 
         $this->info('Storing the file...');
